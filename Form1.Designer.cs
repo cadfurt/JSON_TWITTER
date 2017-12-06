dc76d646b278e24433cfcfc3b4cc89486a7fd9dd
cadfurt <cadfurt@users.noreply.github.com>
2017-12-06 12:31:22 -0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace C_JSON_VISUAL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDiretorio = new System.Windows.Forms.TextBox();
+            this.btnFiltrar = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCsv = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bgWorkerIndeterminada = new System.ComponentModel.BackgroundWorker();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDiretorio
+            // 
+            this.txtDiretorio.Location = new System.Drawing.Point(128, 20);
+            this.txtDiretorio.Name = "txtDiretorio";
+            this.txtDiretorio.Size = new System.Drawing.Size(208, 20);
+            this.txtDiretorio.TabIndex = 0;
+            this.txtDiretorio.Text = "C:\\Users\\medilab\\Documents\\Visual Studio 2017\\Projects\\C_JSON_VISUAL\\tweeter_comp" +
+    "leto.json";
+            // 
+            // btnFiltrar
+            // 
+            this.btnFiltrar.Location = new System.Drawing.Point(342, 18);
+            this.btnFiltrar.Name = "btnFiltrar";
+            this.btnFiltrar.Size = new System.Drawing.Size(75, 23);
+            this.btnFiltrar.TabIndex = 1;
+            this.btnFiltrar.Text = "Filtrar";
+            this.btnFiltrar.UseVisualStyleBackColor = true;
+            this.btnFiltrar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 81);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(405, 262);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Caminho do JSON:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(124, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Preview dos dados";
+            // 
+            // btnCsv
+            // 
+            this.btnCsv.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCsv.Location = new System.Drawing.Point(146, 349);
+            this.btnCsv.Name = "btnCsv";
+            this.btnCsv.Size = new System.Drawing.Size(127, 52);
+            this.btnCsv.TabIndex = 6;
+            this.btnCsv.Text = "CSV";
+            this.btnCsv.UseVisualStyleBackColor = true;
+            this.btnCsv.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 469);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(405, 20);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 453);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
+            // bgWorkerIndeterminada
+            // 
+            this.bgWorkerIndeterminada.WorkerSupportsCancellation = true;
+            this.bgWorkerIndeterminada.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWorkerIndeterminada_DoWork);
+            this.bgWorkerIndeterminada.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWorkerIndeterminada_RunWorkerCompleted);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(302, 349);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(115, 52);
+            this.btnCancelar.TabIndex = 10;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 501);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnCsv);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnFiltrar);
+            this.Controls.Add(this.txtDiretorio);
+            this.Name = "Form1";
+            this.Text = "Ler JSON Twitter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDiretorio;
+        private System.Windows.Forms.Button btnFiltrar;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCsv;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker bgWorkerIndeterminada;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Web.Script.Serialization;
+
+namespace C_JSON_VISUAL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnCancelar.Enabled = false;
+            btnCsv.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnCancelar.Enabled = true;
+            btnFiltrar.Enabled = false;
+            bgWorkerIndeterminada.RunWorkerAsync();
+           
+            //define a progressBar para Marquee
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            progressBar1.MarqueeAnimationSpeed = 4;
+
+            //informa que a tarefa esta sendo executada.
+            label3.Text = "Processando...";
+
+        }
+
+        delegate void teste(string path);
+
+
+        private void populaTxt(string path)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            path = txtDiretorio.Text;
+
+            dynamic textoToJson(string pArquivo)
+            {
+                using (StreamReader arquivo = File.OpenText(pArquivo))
+                using (JsonTextReader leitor = new JsonTextReader(arquivo))
+                {
+                    dynamic resultado = JToken.ReadFrom(leitor);
+                    return resultado;
+                }
+            }
+
+            dynamic json_tweeter = textoToJson(@path);
+
+            StringBuilder sb = new StringBuilder();
+            
+                richTextBox1.Clear();
+                richTextBox1.Text = "user name" + " | " + "created_at" + " | " + "favourites_count" + " | " + "followers_count" + " | " + "friends_count" + " | " +
+                                "statuses_count" + " | " + "text" + " || " + "\n";
+
+                for (int i = 0; i < json_tweeter.Count; i++)
+                {
+                    richTextBox1.Text += (json_tweeter[i]["user"]["name"] + " | " +
+                                            json_tweeter[i]["created_at"] + " | " +
+                                            json_tweeter[i]["user"]["favourites_count"] + " | " +
+                                            json_tweeter[i]["user"]["followers_count"] + " | " +
+                                            json_tweeter[i]["user"]["friends_count"] + " | " +
+                                            json_tweeter[i]["user"]["statuses_count"] + " | " +
+                                            json_tweeter[i]["text"] + " || " +
+                                            "\n");
+
+                    Console.WriteLine("id: " + json_tweeter[i]["id"]);
+                    Console.WriteLine(json_tweeter[i]["user"]["name"]);
+                }
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string path2 = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory.ToString()) + "tweeter.CSV";
+            System.IO.File.WriteAllText(path2, richTextBox1.Text);
+            MessageBox.Show("Arquivo criado em : " + path2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.ShowDialog();
+        }
+
+        private void bgWorkerIndeterminada_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            //executa a tarefa a primeira vez
+            populaTxt(txtDiretorio.Text);
+            //Verifica se houve uma requisição para cancelar a operação.
+            if (bgWorkerIndeterminada.CancellationPending)
+            {
+                //se sim, define a propriedade Cancel para true
+                //para que o evento WorkerCompleted saiba que a tarefa foi cancelada.
+                MessageBox.Show("CANCELAAA");
+               e.Cancel = true;
+                return;
+            }
+
+           
+        }
+
+        private void bgWorkerIndeterminada_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //Caso cancelado...
+            if (e.Cancelled)
+            {
+                // reconfigura a progressbar para o padrao.
+                progressBar1.MarqueeAnimationSpeed = 0;
+                progressBar1.Style = ProgressBarStyle.Blocks;
+                progressBar1.Value = 0;
+
+                //caso a operação seja cancelada, informa ao usuario.
+                label3.Text = "Operação Cancelada pelo Usuário!";
+
+                //habilita o botao cancelar
+                btnCancelar.Enabled = true;
+                
+            }
+            else if (e.Error != null)
+            {
+                //informa ao usuario do acontecimento de algum erro.
+                label3.Text = "Aconteceu um erro durante a execução do processo!";
+
+                // reconfigura a progressbar para o padrao.
+                progressBar1.MarqueeAnimationSpeed = 0;
+                progressBar1.Style = ProgressBarStyle.Blocks;
+                progressBar1.Value = 0;
+            }
+            else
+            {
+                //informa que a tarefa foi concluida com sucesso.
+                label3.Text = "Tarefa Concluida com sucesso!";
+
+                //Carrega todo progressbar.
+                progressBar1.MarqueeAnimationSpeed = 0;
+                progressBar1.Style = ProgressBarStyle.Blocks;
+                progressBar1.Value = 100;
+                btnCsv.Enabled = true;
+                btnCancelar.Enabled = false;
+                btnFiltrar.Enabled = false;
+            }
+            
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            if (bgWorkerIndeterminada.IsBusy)
+            {
+                // notifica a thread que o cancelamento foi solicitado.
+                // Cancela a tarefa DoWork 
+                bgWorkerIndeterminada.CancelAsync();
+                               
+            }
+
+            //desabilita o botão cancelar.
+            btnCancelar.Enabled = false;
+            label3.Text = "Cancelando...";
+        }
+    }
+}
